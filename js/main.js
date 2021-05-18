ce2cbd708a3a6c3d701cfb4c13055081663e4213
AppTv <apptv.contato@gmail.com>
2021-05-18 17:10:52 -0300

    Update Aula 01
    Update Aula 01

---
+++ js/main.js
@@ -0,0 +1,36 @@
+/*
+var nome = "Raphael Silva";
+var idade = 35;
+var idade2 = 10;
+var n1 = 3;
+var n2 = 5;
+var lista = ["maça", "pera", "laranja"];
+var fruta = {nome: "Maça", cor: "Vermelha"}
+var frutas = [{nome: "Maça", cor: "Vermelha"}, {nome: "Uva", cor: "Roxa"}]
+*/
+
+
+
+var frase = "Japão é o melhor time do mundo"
+//alert(nome + " tem " + idade + " anos"); // conctenando conteudos
+//alert(frase.replace("Japão", "Brasil")); //altera texto
+//alert(frase.toUpperCase()); //texto em mauisculo
+//alert(frase.toLowerCase()); //texto em minusculo
+//alert(n1 * n2);
+//alert(lista);
+//lista.push("uva") //adiciona elemento
+//lista.pop(); //retira o ultimo elemento
+//alert(lista.length); //tamanho do array
+//alert(lista.reverse()); //elementos do array em ordem contraria
+//alert(lista.toString()); //converte array em string
+//console.log(lista.toString()); //converte array em string
+//console.log(lista.join(" - ")); //Adicionar elemento (separador) ao array convertido em string
+//console.log(fruta);
+//console.log(frutas);
+
+var idade = prompt("Qual a sua idade");
+if (idade >= 18){
+    alert("Maior de idade, conteudo liberado")
+}else{
+    alert("Menor de idade, conteudo bloqueado")
+};
