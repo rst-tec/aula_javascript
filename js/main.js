da0be85eb3c2b562c8de1b8f8c717ca399dbee4f
AppTv <apptv.contato@gmail.com>
2021-05-19 17:11:53 -0300

    Update Aula 02
    Update Aula 02

--- js/main.js
+++ js/main.js
@@ -28,9 +28,62 @@ var frase = "Japão é o melhor time do mundo"
 //console.log(fruta);
 //console.log(frutas);
 
+/*
+//ESTRUTURA CONDICIONAL
 var idade = prompt("Qual a sua idade");
 if (idade >= 18){
     alert("Maior de idade, conteudo liberado")
 }else{
     alert("Menor de idade, conteudo bloqueado")
 };
+*/
+
+/*
+//ESTRUTURA DE REPETIÇÃO
+var numero = 0;
+while (numero <=5){
+    console.log(numero);
+    alert("O Numero atual é: " + numero);
+    numero++;
+};
+*/
+
+/*
+//ESTRUTURA DE REPETIÇÃO
+var numero;
+for(numero = 0; numero <=5; numero++){
+    alert(numero);
+};
+*/
+
+/*
+var d = new Date();
+alert(d.getDay());
+alert(d.getHours());
+alert(d.getMinutes());
+alert(d.getDate());
+*/
+
+/*
+var d = new Date();
+console.log("hoje é" + d.getMoth() + "/" +(d.getDate()+1));
+*/
+
+/*
+function soma(n1, n2){
+    return n1 + n2;
+}
+alert(soma(5,10));
+
+*/
+
+var idade = prompt("Qual a sua idade");
+
+function clicou(){
+    alert("Sua idade é: " + idade);
+};
+
+
+
+
+
